e60f712871bc3d88280574c50b468a4fd0b3a925
aakash2018 <aakashprajapat2020@gmail.com>
2023-10-24 11:21:03 +0530

    new changes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
 
+import { en_US } from 'ng-zorro-antd/i18n';
+
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb'
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { ReactiveFormsModule } from '@angular/forms';
+registerLocaleData(en);
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzTableModule,
+    NzDividerModule,
+    NzModalModule,
+    NzBreadCrumbModule,
+    NzCheckboxModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
